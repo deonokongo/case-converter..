f0299d64c61f0921fd0c0a45a51a49a4836c305a
deonokongo <lilian.deon29@gmail.com>
2022-11-25 00:26:36 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+         int num1 = 50;
+        int num2 = 40;
+         int sum = num1 + num2;
+        System.out.println("Sum is:" + sum);
+    }
+}
